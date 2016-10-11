a4e88947c30cc2ce8a1dc1f2376ad42b940b31b7
Ooghe Nicolas <zorganico@gmail.com>
2016-10-11 13:51:25 +0200

    Update ex.php

--- ex.php
+++ ex.php
@@ -3,4 +3,11 @@
   include("index.php");
   file_get_contents("en_lang.php");
   file_get_contents("index.php");
+  $url = './index.php';
+  $curl = curl_init();
+  curl_setopt($curl, CURLOPT_URL, $url);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($curl, CURLOPT_HEADER, false);
+  $data = curl_exec($curl);
+  curl_close($curl);
 ?>
