520e4fd69c6b90f47089cc50230c423c85e3b6b8
Ooghe Nicolas <zorganico@gmail.com>
2016-10-11 14:01:29 +0200

    Update ex.php

--- ex.php
+++ ex.php
@@ -1,6 +1,3 @@
 <?php
-  print_r(scandir('.'));
-  include("index.php");
-  readfile("http://challenge01.root-me.org/web-serveur/ch13/index.php");
-  file_get_contents("http://challenge01.root-me.org/web-serveur/ch13/index.php");
+  readfile("index.php");
 ?>
